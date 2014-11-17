df92a9155c2d54742d359a31c5cd37c1e698a35d
yuichi matsuoka <anything12new14@gmail.com>
2014-11-17 13:49:30 +0900

    extract directives

--- js/controller.js
+++ js/controller.js
@@ -106,7 +106,6 @@ app.factory('Insta', function($http) {
             parameter.count = 20;
 
             $http.get(url, {params : parameter}).success(function(data) {
-                    console.log(data);
                     this.next_url = data.pagination.next_url;
                     var items = data.data;
                     if(items){
@@ -148,31 +147,6 @@ app.factory('Insta', function($http) {
     return Insta;
 });
 
-app.directive('repeatDone', function() {
-    return function(scope, element, attrs) {
-        if (scope.$last) { // all are rendered
-            scope.$eval(attrs.repeatDone);
-        }
-    }
-});
-
-app.directive('ngElementReady', [function() {
-
-    return {
-        priority: -1000,
-        restrict: "A",
-        link: function($scope, $element, $attributes) {
-            $scope.$eval($attributes.ngElementReady); // execute the expression in the attribute.
-
-            $(".instpics").turnBox({
-                width: 320,
-                height: 320,
-                axis: "X",
-                duration: 400,
-            });
-        }
-    };
-}]);
 
 function switchColor(target) {
 
